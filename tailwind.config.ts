2fdef17033d2a4ca8e61640194eb2d6d1d1a8940
Huzaifa Irfan <91394134+Huzawfaw@users.noreply.github.com>
2025-08-15 16:19:26 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,16 @@
-import type { Config } from "tailwindcss";
-export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: {} },
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class', // allow manual dark mode toggle
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-} satisfies Config;
+}
+
+export default config
